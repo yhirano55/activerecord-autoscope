195ce6937ed1d0b72366d14313bc6963eb8de8c8
Yoshiyuki Hirano <yhirano@me.com>
2019-05-06 23:03:23 +0900

    Update Railtie

--- lib/activerecord/auto_scope/railtie.rb
+++ lib/activerecord/auto_scope/railtie.rb
@@ -3,19 +3,11 @@
 module ActiveRecord
   module AutoScope
     class Railtie < ::Rails::Railtie # :nodoc:
-      config.before_initialize do
+      config.after_initialize do
         ActiveSupport.on_load :active_record do
           ActiveRecord::Base.extend ::ActiveRecord::AutoScope::ScopeMethods
+          ActiveRecord::Base.prepend ::ActiveRecord::AutoScope::HookMethods
         end
-      end
-
-      config.to_prepare do
-        ::ActiveRecord::AutoScope.tracer.enable if ::ActiveRecord::AutoScope.config.auto_define_scopes
-      end
-
-      # TODO: Disable trace point on development and test
-      config.after_initialize do
-        ::ActiveRecord::AutoScope.tracer.disable if ::ActiveRecord::AutoScope.config.auto_define_scopes && ::Rails.application.config.eager_load
       end
     end
   end
